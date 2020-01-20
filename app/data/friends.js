0e729450585d10be47e7f5f60e3b442eaea13632
misosoup17 <misagaspar@icloud.com>
2020-01-19 21:52:55 -0800

    updated
    I had to delete the repository because I couldnt deploy through heroku since I had to many staging names after I deleted my heroku app and made a new one

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+var friends = [{
+    name: "Erin Peters",
+    photo: "https://images.pexels.com/photos/3420044/pexels-photo-3420044.jpeg?cs=srgb&dl=woman-with-light-on-face-3420044.jpg&fm=jpg",
+    scores: [4,4,3,2,1]
+}, {
+    name: "Renee Lana",
+    photo: "https://www.pexels.com/photo/shallow-focus-photo-of-woman-using-game-boy-3491940/",
+    scores: [1,2,3,4,5]
+}, {
+    name: "Ralph Lo",
+    photo: "https://www.pexels.com/photo/black-man-reading-a-newspaper-3473492/",
+    scores: [5,4,3,2,1]    
+}, {
+    name: "Joan Jones",
+    photo: "https://www.pexels.com/photo/woman-in-brown-jacket-and-black-shirt-3484683/",
+    scores: [4,3,2,1,5]
+}, {
+    name:"Terry Maitland",
+    photo: "https://www.pexels.com/photo/woman-standing-on-the-seashore-3439243/",
+    scores: [3,2,1,5,4]
+}];
+
+module.exports = friends;
